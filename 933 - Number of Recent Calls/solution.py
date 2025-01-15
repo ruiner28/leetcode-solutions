87e5bc74c67e1356ec401b70659dfcff33192761
ruiner28 <gpawar6444@gmail.com>
2025-01-15 12:44:14 -0800

    Problem 993

---
+++ 933 - Number of Recent Calls/solution.py
@@ -0,0 +1,19 @@
+from collections import deque
+
+class RecentCounter:
+    def __init__(self):
+        self.queue = deque()
+
+    def ping(self, t: int) -> int:
+        while self.queue and self.queue[0] < t - 3000:
+            self.queue.popleft()
+        
+        self.queue.append(t)
+        return len(self.queue)
+
+
+# Your RecentCounter object will be instantiated and called as such:
+# obj = RecentCounter()
+# param_1 = obj.ping(t)
+
+#In Python, we're using collections.deque to implement the queue. This data structure allows us to perform dequeue operations from the front in O(1)
