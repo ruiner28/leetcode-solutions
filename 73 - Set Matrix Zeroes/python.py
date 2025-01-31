a0f879918c76465fb4358d75ef8b754ae7abcbc1
ruiner28 <gpawar6444@gmail.com>
2025-01-31 13:20:55 -0800

    Problem 73 approach 2 added

--- 73 - Set Matrix Zeroes/python.py
+++ 73 - Set Matrix Zeroes/python.py
@@ -20,3 +20,33 @@ class Solution:
             if j in zeroth_col:
                 for i in range(r):
                     matrix[i][j] = 0
+
+# Time Complexity: O(m*n)
+# Space Complexity: O(m+n)
+# Approach 2
+'''
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        r = len(matrix)
+        c = len(matrix[0])
+        zeroth_row = False
+        for i in range(r):
+            if matrix[i][0] == 0:
+                zeroth_row = True
+            for j in range(1, c):
+                if matrix[i][j] == 0:
+                    matrix[i][0] = 0
+                    matrix[0][j] = 0
+        for i in range(1, r):
+            for j in range(1, c):
+                if matrix[i][0] == 0 or matrix[0][j] == 0:
+                    matrix[i][j] = 0
+        if zeroth_row:
+            for i in range(r):
+                matrix[i][0] = 0
+    '''             
+# Time Complexity: O(m*n)
+# Space Complexity: O(1)
