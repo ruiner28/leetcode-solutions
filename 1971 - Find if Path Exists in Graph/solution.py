e46f7055091ddd026a20eebd3c82c34fb337c17a
ruiner28 <gpawar6444@gmail.com>
2025-02-05 23:01:50 -0800

    Iterative solution using stack

--- 1971 - Find if Path Exists in Graph/solution.py
+++ 1971 - Find if Path Exists in Graph/solution.py
@@ -16,3 +16,28 @@ class Solution:
             return False
         visited = set()        
         return dfs(source, visited)
+    
+
+# Iterative solution using stack
+'''
+class Solution:
+    def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
+        
+        def dfs(node):
+            stack = [node]
+
+            while stack:
+                node = stack.pop()
+                for nei in graph[node]:
+                    if nei not in visited:
+                        stack.append(nei)
+                        visited.add(nei)
+        visited = set()
+        visited.add(source)
+        graph = defaultdict(list)
+        for i, j in edges:
+            graph[i].append(j)
+            graph[j].append(i)
+        dfs(source)
+        return destination in visited
+'''    
