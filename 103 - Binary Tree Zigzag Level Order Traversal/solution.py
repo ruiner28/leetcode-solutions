d09120c310a7c754c5fa12f3dbd023f29c4a912c
ruiner28 <gpawar6444@gmail.com>
2025-02-23 14:16:57 -0800

    Problem 103

---
+++ 103 - Binary Tree Zigzag Level Order Traversal/solution.py
@@ -0,0 +1,29 @@
+class Solution:
+    def zigzagLevelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if not root:
+            return []
+        res = []
+        q = deque([root])
+        reverse = True
+
+        while q:
+            new_q = deque()
+            level = []
+            for node in q:
+                level.append(node.val)
+                if reverse:
+                    if node.left:
+                        new_q.appendleft(node.left)
+                    if node.right:
+                        new_q.appendleft(node.right)
+                else:
+                    if node.right:
+                        new_q.appendleft(node.right)
+                    if node.left:
+                        new_q.appendleft(node.left)
+            
+            res.append(level)
+            q = new_q
+            reverse = not reverse
+        
+        return res
